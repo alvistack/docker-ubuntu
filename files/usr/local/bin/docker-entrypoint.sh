ba3b39c0b60a36deef0acbe8bbe935f1028254ff
Wong Hoi Sing Edison <hswong3i@gmail.com>
2018-10-11 15:08:25 +0800

    ssh-keygen -A

--- files/usr/local/bin/docker-entrypoint.sh
+++ files/usr/local/bin/docker-entrypoint.sh
@@ -6,9 +6,8 @@ set -o xtrace
 screen -dm bash -c "ansible-galaxy install --force --roles-path /etc/ansible/roles --role-file /etc/ansible/ansible-role-requirements.yml && ansible-playbook /etc/ansible/playbooks/bootstrap.yml"
 
 # Regenerate OpenSSH host keys
-for type in dsa ecdsa ed25519 rsa; do
-    yes y | ssh-keygen -N '' -t ${type} -f /etc/ssh/ssh_host_${type}_key
-done
+rm -rf /etc/ssh/ssh_host_*
+ssh-keygen -A
 
 # Running sshd in foreground
 exec /usr/sbin/sshd -eD
