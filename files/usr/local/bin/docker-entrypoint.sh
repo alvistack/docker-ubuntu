25f8ac18698687c7a588cb97dabc9cd22cc2877e
Wong Hoi Sing Edison <hswong3i@gmail.com>
2018-11-20 09:48:33 +0800

    - Add /root/.ssh/config   - Set correct permission for /root/.ssh during bootup

--- files/usr/local/bin/docker-entrypoint.sh
+++ files/usr/local/bin/docker-entrypoint.sh
@@ -6,5 +6,9 @@ set -o xtrace
 rm -rf /etc/ssh/ssh_host_*
 ssh-keygen -A
 
+# Set correct permission for /root/.ssh
+chmod 0700 /root/.ssh
+chmod 0600 /root/.ssh/*
+
 # Running sshd in foreground
 exec /usr/sbin/sshd -eD
